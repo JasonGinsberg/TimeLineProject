5c16eeb58a4480418223bcfe999d1d7170b4dad7
Jason Ginsberg <Jason@jason-ginsbergs-macbook-pro-3.local>
2013-03-31 10:59:17 -0400

    Initial Commit

---
+++ Delegate/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Delegate
+//
+//  Created by Jason Ginsberg on 3/31/13.
+//  Copyright (c) 2013 Jason Ginsberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
