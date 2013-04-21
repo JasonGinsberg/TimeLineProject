290cc0ba1e21568f889cd1a764ae936ca83bc22a
Jason Ginbserg <ginsdaman@gmail.com>
2013-04-21 15:23:05 -0400

    update

---
+++ Delegate/JsonF/JsonF.h
@@ -0,0 +1,13 @@
+//
+//  JsonF.h
+//  JsonF
+//
+//  Created by Jason Ginsberg on 3/31/13.
+//  Copyright (c) 2013 Jason Ginsberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JsonF : NSObject
+
+@end
---
+++ Delegate/JsonF/JsonF.m
@@ -0,0 +1,13 @@
+//
+//  JsonF.m
+//  JsonF
+//
+//  Created by Jason Ginsberg on 3/31/13.
+//  Copyright (c) 2013 Jason Ginsberg. All rights reserved.
+//
+
+#import "JsonF.h"
+
+@implementation JsonF
+
+@end
